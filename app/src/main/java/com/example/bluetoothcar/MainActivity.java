3727a2f15a7651146a2ddfb56bf7770e87fd75ae
YiRuYang <1287865814@qq.com>
2017-08-24 10:25:43 +0800

    First commit

---
+++ app/src/main/java/com/example/bluetoothcar/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.bluetoothcar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //取消标题栏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //取消状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+    }
+
+}
