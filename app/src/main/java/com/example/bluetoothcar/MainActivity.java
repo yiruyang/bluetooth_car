eebcf1f21fa05c2c825f3bac1fb70dcc2eea3d7e
YiRuYang <1287865814@qq.com>
2017-08-25 18:10:38 +0800

    bluetoothconnect

--- app/src/main/java/com/example/bluetoothcar/MainActivity.java
+++ app/src/main/java/com/example/bluetoothcar/MainActivity.java
@@ -7,20 +7,26 @@ import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.io.OutputStream;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import static android.R.layout.simple_spinner_dropdown_item;
 
@@ -32,13 +38,16 @@ public class MainActivity extends Activity {
     BluetoothSocket bluetoothSocket;
     OutputStream outputStream = null;
     private int REQUEST_ENABLE_BT = 2;
+    String name;
 
     private Switch bluetooth_on_off;
     private Button deviceSearch;
     private Spinner bluetooth_spinner;
     private Button bluetooth_connect;
     private List<String> deviceList = new ArrayList<>();
-    private ArrayAdapter<String> arrayAdapter ;
+    private ArrayAdapter<String> mArrayAdapter ;
+    private MyBroadcastReceiver mFoundReceiver;
+    private TextView textView_bluetooth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +62,7 @@ public class MainActivity extends Activity {
         deviceSearch = (Button) findViewById(R.id.bluetooth_search);
         bluetooth_spinner = (Spinner) findViewById(R.id.bluetooth_spinner);
         bluetooth_connect = (Button) findViewById(R.id.bluetooth_connect);
+        textView_bluetooth = (TextView) findViewById(R.id.textView_bluetooth);
         bluetooth_on_off.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -73,24 +83,101 @@ public class MainActivity extends Activity {
         deviceSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                BroadcastReceiver mFoundReceiver = new BroadcastReceiver() {
-                    @Override
-                    public void onReceive(Context context, Intent intent) {
-                        String action = intent.getAction();
-                        //找到设备
-                        if (BluetoothDevice.ACTION_FOUND.equals(action)){
-                            bluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                            if (bluetoothDevice.getBondState() != bluetoothDevice.BOND_BONDED){
-                                deviceList.add(bluetoothDevice.getName()+bluetoothDevice.getAddress());
-                                arrayAdapter.setDropDownViewResource(simple_spinner_dropdown_item);
-                                bluetooth_spinner.setAdapter(arrayAdapter);
-
-                            }
-                        }
-                    }
-                };
+                mArrayAdapter.clear();
+                // 寻找蓝牙设备，android会将查找到的设备以广播形式发出去
+                bluetoothAdapter.startDiscovery();
             }
         });
+        mArrayAdapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_spinner_item,deviceList);
+        mArrayAdapter.setDropDownViewResource(simple_spinner_dropdown_item);
+        bluetooth_spinner.setAdapter(mArrayAdapter);
+
+        bluetooth_spinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                textView_bluetooth.setText(deviceList.get(position));
+                //设置显示当前选择的项
+                parent.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                arg0.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        bluetooth_connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (bluetoothDevice.getName().equalsIgnoreCase(name)) {
+                    // 搜索蓝牙设备的过程占用资源比较多，一旦找到需要连接的设备后需要及时关闭搜索
+                    bluetoothAdapter.cancelDiscovery();
+                    // 获取蓝牙设备的连接状态
+                    int connectState = bluetoothDevice.getBondState();
+                    switch (connectState) {
+                        // 未配对
+                        case BluetoothDevice.BOND_NONE:
+                            // 配对
+                            try {
+                                Method createBondMethod = BluetoothDevice.class.getMethod("createBond");
+                                createBondMethod.invoke(bluetoothDevice);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                            break;
+                        // 已配对
+                        case BluetoothDevice.BOND_BONDED:
+                            try {
+                                // 连接
+                                connect(bluetoothDevice);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            break;
+                    }
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFoundReceiver = new MyBroadcastReceiver();
+        //广播接收器
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BluetoothDevice.ACTION_FOUND);
+        // 注册广播接收器，接收并处理搜索结果
+        this.registerReceiver(mFoundReceiver, intentFilter);
+    }
+
+    public class MyBroadcastReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            //找到设备
+            if (BluetoothDevice.ACTION_FOUND.equals(action)){
+                bluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                Toast.makeText(MainActivity.this, "发现蓝牙设备！", Toast.LENGTH_SHORT).show();
+                if (bluetoothDevice.getBondState() != bluetoothDevice.BOND_BONDED){
+                    deviceList.add(bluetoothDevice.getName());
+                    mArrayAdapter.notifyDataSetChanged();
+                }
+
+            }
+        }
+    }
+
+    private void connect(BluetoothDevice device) throws IOException {
+        // 固定的UUID
+        final String SPP_UUID = "00001101-0000-1000-8000-00805F9B34FB";
+        UUID uuid = UUID.fromString(SPP_UUID);
+        BluetoothSocket socket = device.createRfcommSocketToServiceRecord(uuid);
+        socket.connect();
     }
 
 }
